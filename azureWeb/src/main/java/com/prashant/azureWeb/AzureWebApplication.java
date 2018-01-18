2fe699e6228e41b97b8bd6236d8b49a547e48d24
Prashant Kumar Tripathi <Prashant.Tripathi@NIIT-Tech.com>
2018-01-18 13:13:42 +0530

    Added all files

---
+++ azureWeb/src/main/java/com/prashant/azureWeb/AzureWebApplication.java
@@ -0,0 +1,12 @@
+package com.prashant.azureWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureWebApplication.class, args);
+	}
+}
